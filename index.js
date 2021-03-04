4b400ef023888f34a2ad4df60cb2c8c50a694440
hamza15 <sardan5@vt.edu>
2021-03-04 00:53:42 -0500

    commit tree 45cb6cd661659267e4d09e564a8765eb32f78de4

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 class Formatter {
   //add static methods here
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9 '-]/g, '')
+  }
+
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static titleize(string) {
+    const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+
+    let stringArray = string.split(" ")
+    let capArray = []
+
+    for (let i = 0; i < stringArray.length; i++) {
+      if (i == 0) {
+        capArray.push(this.capitalize(stringArray[i]))
+      } else {       
+        if (exceptions.includes(stringArray[i])){
+          capArray.push(stringArray[i])
+        } else {
+          capArray.push(this.capitalize(stringArray[i]))
+        }
+      }
+    }
+
+    return capArray.join(" ")
+  }
+  
 }
